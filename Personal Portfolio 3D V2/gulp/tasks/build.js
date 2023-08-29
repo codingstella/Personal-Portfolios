9a4c88d242282662a9621f71c9af49a29ecdae2f
Coding Stella <127761646+codingstella@users.noreply.github.com>
2023-08-29 17:35:29 +0530

    Add files via upload

---
+++ Personal Portfolio 3D V2/gulp/tasks/build.js
@@ -0,0 +1,28 @@
+var gulp        = require('gulp');
+var runSequence = require('run-sequence');
+var config      = require('../config');
+
+function build(cb) {
+    runSequence(
+        'clean',
+        'sprite:svg',
+        'svgo',
+        'sass',
+        'pug',
+        'js',
+        'copy',
+        cb
+    );
+}
+
+gulp.task('build', function(cb) {
+    config.setEnv('production');
+    config.logEnv();
+    build(cb);
+});
+
+gulp.task('build:dev', function(cb) {
+    config.setEnv('development');
+    config.logEnv();
+    build(cb);
+});
---
+++ Personal Portfolio 3D V2/gulp/tasks/server.js
@@ -0,0 +1,35 @@
+var gulp   = require('gulp');
+var server = require('browser-sync').create();
+var util   = require('gulp-util');
+var config = require('../config');
+
+// in CL 'gulp server --open' to open current project in browser
+// in CL 'gulp server --tunnel siteName' to make project available over http://siteName.localtunnel.me
+
+gulp.task('server', function() {
+    server.init({
+        server: {
+            baseDir: !config.production ? [config.dest.root, config.src.root] : config.dest.root,
+            directory: false,
+            serveStaticOptions: {
+                extensions: ['html']
+            }
+        },
+        files: [
+            config.dest.html + '/*.html',
+            config.dest.css + '/*.css',
+            config.dest.img + '/**/*'
+        ],
+        port: util.env.port || 3000,
+        logLevel: 'info', // 'debug', 'info', 'silent', 'warn'
+        logConnections: false,
+        logFileChanges: true,
+        open: true,
+        notify: false,
+        ghostMode: false,
+        online: true,
+        tunnel: util.env.tunnel || null
+    });
+});
+
+module.exports = server;
